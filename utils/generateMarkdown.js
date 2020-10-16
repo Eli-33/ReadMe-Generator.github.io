e4fcf0cb57de08be79278831b4f7125fd93ecf23
Eli-33 <elijam1608@gmail.com>
2020-10-16 11:45:44 +1100

    Update generateMarkdown.js
    added some code for generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,41 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# Project Title: ${data.title}
+
+## Project Description : ${data.desc}
+
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Test](#test)
+  * [Questions](#questions)
+  * [License](#license)
+  * [Author] (#Author)
+  * [Badges](#badges)
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributors
+  ${data.contributors}
+
+  ## Test
+  
+
+## Questions
+  If you have any questions, contact ${data.username} on GitHub.
+
+  ## License
+ 
+
+
+  ## Author 
+  
+
+  ## Badges
+
 
 `;
 }
