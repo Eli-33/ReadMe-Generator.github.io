374b1057c23716d8be557062ba784324d1bbac84
Eli-33 <elijam1608@gmail.com>
2020-10-19 10:21:55 +1100

    Update index.js

--- index.js
+++ index.js
@@ -80,10 +80,6 @@ const questions = [
     },
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
 // function to initialize program
 function init() {
     inquirer.prompt(questions).then(answers =>{
